e25d4383e5b8845217c9c06fe8b8ad7e8bba3107
attiliopet <127427372+attiliopet@users.noreply.github.com>
2023-03-16 17:45:48 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -31,3 +31,38 @@ plot(l2011$B4_sre, col=cl)# $ mi lega le due cose ed è la stessa cosa che selez
 
 nir <- l2011[[4]] # assegno la banda 4 a un nome così da smplificarmi la vita. nir <- l2011$B4_sre
 plot(nir, col=cl)
+
+# Exercise 
+dev.off() #chiudo il grafico
+pdf("myfirstgraph.pdf") #per esportare grafici e metterli su una cartella esterna a R: prima do il nome al file tra virgolette che esco d R
+#poi vado a plottare tutto quello che voglio, così me lo insersce all'intno del pdf nella cartella lab perchè ho fatto il st della workingdyrectori prcedentemente
+plot(l2011[[4]], col=cl)
+dev.off()
+#mf= multiframe per plottare un file uno sl di sottto dell'altro senza che si soprappongono
+par(mfrow=c(2,1)) # così apro una finestra grafica con 2 colonne e una riga
+plot(l2011[[3]], col=cl)
+plot(l2011[[4]], col=cl) # inserisco i due elementi da plottare
+
+# plotting the first 4 layers / bands
+
+par(mfrow=c(2,2))
+# assegno un palette diversa a ogni elemento che mi formeranno il mio multiframe
+# blue 
+clb <- colorRampPalette(c("blue4", "blue2", "light blue"))(100)
+plot(l2011[[1]], col=clb)
+# Green
+clg <- colorRampPalette(c("chartreuse4", "chartreuse2", "chartreuse"))(100)
+plot(l2011[[2]], col=clg)
+# Red
+clr <- colorRampPalette(c("brown1", "brown2", "brown4"))(100)
+plot(l2011[[3]], col=clr)
+# Infrared
+cli <- colorRampPalette(c("darkorange", "darkorange2", "darkorange4"))(100)
+plot(l2011[[4]], col=cli)
+# se voglio fare un nuovo multiframe ma formato con un numero differente di righe o colonne devo prima chiudere il multiframe preceedentemente aperto che se no lui mi rimane in quella modalità
+# chiudo la modalità multiframe precentemente usata, usando la funzione dev.off()
+
+
+
+
+
